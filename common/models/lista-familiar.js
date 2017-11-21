99ee20d495dbc9d5a78a6a3e0ca32c7907e8f78c
Oualid Amallah <walidamallah@gmail.com>
2017-11-21 09:14:45 +0100

    asociar propitario es el usuario login

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -2,4 +2,8 @@
 
 module.exports = function(Listafamiliar) {
 
+    Listafamiliar.beforeRemote('create', function (context, centro, next) {
+        context.args.data.owner = context.req.accessToken.userId;
+        next();
+    });
 };
