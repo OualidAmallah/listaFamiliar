4df2f2810ccd4175635dcc36382607c83a8848aa
Oualid Amallah <walidamallah@gmail.com>
2017-11-21 11:00:35 +0100

    Asegnar a usario.listfamiliaID id de listaFimiliar

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -2,8 +2,24 @@
 
 module.exports = function(Listafamiliar) {
 
-    Listafamiliar.beforeRemote('create', function (context, centro, next) {
+    Listafamiliar.beforeRemote('create', function (context, listafamiliar, next) {
         context.args.data.owner = context.req.accessToken.userId;
         next();
     });
+
+    Listafamiliar.afterRemote('create', function (context, listafamiliar, next) {
+        var app = Listafamiliar.app;
+        var  Usuario= app.models.Usuario;
+        var  userId=context.req.accessToken.userId
+
+        Usuario.findById(userId, function(err, usuario){
+            if(err) next(err) 
+          //  if (usuario){
+                usuario.listaFamiliarId=listafamiliar.id;
+                usuario.save();
+                next();
+           // }
+        });
+    });
+
 };
