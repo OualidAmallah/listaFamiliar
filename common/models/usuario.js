12ada335c904b711b9c6ee4cb79279016f646526
Oualid Amallah <walidamallah@gmail.com>
2017-11-27 07:50:17 +0100

    rechazar solucitid

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -11,7 +11,7 @@ Usuario.prototype.aceptarSolicitud = function(context, callback) {
     var aceptarSolicitud;
     var usuarioSolicitante = this;
     var listaId;
-    
+
     var userId=context.req.accessToken.userId
 
     Usuario.findById(userId, function(err, usuarioAutenticado){
@@ -41,5 +41,48 @@ Usuario.prototype.aceptarSolicitud = function(context, callback) {
     });
    
   };
+
+
+  /**
+ * rechazar una solicitud
+ * @param {Function(Error)} callback
+ */
+
+Usuario.prototype.rechazarSolicitudes = function(context ,callback) {
+    var rechazarSolicitud;
+    var usuarioSolicitante = this;
+    var listaId;
+
+    var userId=context.req.accessToken.userId
+
+    Usuario.findById(userId, function(err, usuarioAutenticado){
+        if(err) callback(err); 
+         listaId=usuarioAutenticado.listaFamiliarId;
+
+         usuarioSolicitante.solicitudes.findById(listaId, function(err, listaSolicitada){
+            if(err) callback(err);
+            if(listaSolicitada) { 
+                var listaFamiliaId=listaSolicitada.id;
+                //usuarioSolicitante.listaFamiliarId=listaFamiliaId;
+               // usuarioSolicitante.save();
+            }
+
+            usuarioSolicitante.solicitudes.remove(listaSolicitada, function(err,listaSolicitada) {
+                if(err) callback(err);
+                usuarioSolicitante.save();
+
+                Usuario.find({where: {listaFamiliarId:usuarioAutenticado.listaFamiliarId}}, function(err, lista) {
+                   //console.log(usuarioAutenticado.listaFamiliarId);
+                    if(err) callback(err);
+                    rechazarSolicitud=lista;
+                        callback(null, rechazarSolicitud);
+                });
+              }); 
+        });
+
+    });
+
+  };
+  
 
 };
