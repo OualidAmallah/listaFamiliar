cfaa7b6c9736a9d7884c4f86b30b70910cfa0b11
Oualid Amallah <walidamallah@gmail.com>
2017-11-30 11:05:04 +0100

    limpia lista es dicer pone todas la compras a false del usuario autenticado

--- common/models/producto.js
+++ common/models/producto.js
@@ -1,5 +1,37 @@
 'use strict';
 
 module.exports = function(Producto) {
+   /**
+ * borrar todas la compras de mi lista
+ * @param {object} context me devuelve usuario autenticado
+ * @param {Function(Error, array)} callback
+ */
 
+Producto.limpiarLista = function(context, callback) {
+    var limpiarLista;
+    var producto=this
+    var userId = context.req.accessToken.userId
+    var Usuario = Producto.app.models.Usuario;
+    var listaId;
+
+    Usuario.findById(userId, function (err, usuarioAutenticado) {
+        if (err) callback(err);
+          listaId=usuarioAutenticado.listaFamiliarId;
+
+          Producto.find({where: {listaFamiliarId:listaId}}, function(err, product) {
+            if(err) callback(err);
+            product.forEach(function (producto) {
+                producto.comprar=false;
+                producto.save();
+            })
+            Producto.find({where: {listaFamiliarId:listaId}}, function(err, product) {
+                if(err) callback(err);
+                callback(null, product);
+            });
+           
+        });
+        
+    });
+    
+  };
 };
